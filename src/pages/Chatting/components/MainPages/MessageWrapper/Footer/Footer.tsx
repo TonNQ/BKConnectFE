2c7da1db22a75e1b82b2f0fc117e346b98f04273
TonNQ <nguyenquoctoan12122003@gmail.com>
2023-12-13 09:33:38 +0700

    feat: [F35] get files of room

--- src/pages/Chatting/components/MainPages/MessageWrapper/Footer/Footer.tsx
+++ src/pages/Chatting/components/MainPages/MessageWrapper/Footer/Footer.tsx
@@ -3,10 +3,11 @@
 import { useContext, useEffect, useRef, useState } from 'react'
 import AddCircleRoundedIcon from '@mui/icons-material/AddCircleRounded'
 import SendRoundedIcon from '@mui/icons-material/SendRounded'
+import AttachFileIcon from '@mui/icons-material/AttachFile'
 import { SocketContext } from 'src/contexts/socket.context'
 import { SendSocketData, WebSocketDataType } from 'src/types/socket.type'
 import { AppContext } from 'src/contexts/app.context'
-import { convertToDateTimeServer } from 'src/utils/utils'
+import { convertToDateTimeServer, generateRandomString } from 'src/utils/utils'
 import storage from 'src/utils/firebase'
 import { toast } from 'react-toastify'
 import { ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage'
@@ -22,28 +23,105 @@ export default function Footer() {
 
   // upload ảnh lên firebase
   const fileInputRef = useRef<HTMLInputElement | null>(null)
-  const [file, setFile] = useState<File | null>(null)
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const fileSelectionRef = useRef<HTMLInputElement | null>(null)
+  // const [file, setFile] = useState<File | null>(null)
+  // selected file
+  const [selectedFile, setSelectedFile] = useState<File | null>(null)
+  // const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  //   const selectedFile = event.target.files && event.target.files[0]
+  //   if (selectedFile) {
+  //     setFile(selectedFile)
+  //   }
+  // }
+  const handleChangeFile = (event: React.ChangeEvent<HTMLInputElement>) => {
     const selectedFile = event.target.files && event.target.files[0]
     if (selectedFile) {
-      setFile(selectedFile)
+      setSelectedFile(selectedFile)
     }
   }
+  // useEffect(() => {
+  //   // Thực hiện tải lên Firebase khi có sự thay đổi trong state
+  //   if (file) {
+  //     handleUpload()
+  //   }
+  // }, [file])
   useEffect(() => {
     // Thực hiện tải lên Firebase khi có sự thay đổi trong state
-    if (file) {
-      handleUpload()
+    if (selectedFile) {
+      handleUploadSelectedFile()
     }
-  }, [file])
-  const handleUpload = () => {
-    if (!file) {
-      toast.error('Vui lòng chọn ảnh!')
+  }, [selectedFile])
+  // const handleUpload = () => {
+  //   if (!file) {
+  //     toast.error('Vui lòng chọn ảnh!')
+  //   } else {
+  //     const directory = `${profile?.user_id}/${new Date().toISOString()}/`
+  //     const storageRef = ref(storage, `/Message_Image/${directory}`)
+  //     // progress can be paused and resumed. It also exposes progress updates.
+  //     // Receives the storage reference and the file to upload.
+  //     const uploadTask = uploadBytesResumable(storageRef, file)
+
+  //     uploadTask.on(
+  //       'state_changed',
+  //       () => {
+  //         // const percent = Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100)
+  //         // // update progress
+  //         // setPercent(percent)
+  //       },
+  //       (err) => console.log(err),
+  //       () => {
+  //         // download url
+  //         getDownloadURL(uploadTask.snapshot.ref).then((url) => {
+  //           console.log(url)
+  //           const message: SendSocketData = {
+  //             data_type: WebSocketDataType.IsMessage,
+  //             message: {
+  //               temp_id: convertToDateTimeServer(new Date()),
+  //               room_id: roomInfo?.id,
+  //               message_type: 'Image',
+  //               content: directory,
+  //               root_message_id: null
+  //             }
+  //           }
+  //           wsRef.current?.send(JSON.stringify(message))
+  //           setText('')
+  //           setMessages([
+  //             {
+  //               message_id: 0,
+  //               sender_id: profile?.user_id || null,
+  //               sender_name: 'Bạn',
+  //               sender_avatar: profile?.avatar as string,
+  //               send_time: message.message.temp_id,
+  //               room_id: roomInfo?.id as number,
+  //               message_type: message.message.message_type,
+  //               content: message.message.content,
+  //               root_message_id: message.message.root_message_id,
+  //               root_message_content: null,
+  //               temp_id: message.message.temp_id,
+  //               last_message: 'Bạn: Đã gửi một ảnh đính kèm'
+  //             },
+  //             ...messages
+  //           ])
+  //           setImages((prevImages) => [url, ...prevImages])
+  //         })
+  //       }
+  //     )
+  //   }
+  // }
+  const handleUploadSelectedFile = () => {
+    if (!selectedFile) {
+      toast.error('Vui lòng chọn 1selectedFfile!')
     } else {
-      const directory = `${profile?.user_id}|${new Date().toISOString()}`
-      const storageRef = ref(storage, `/Message_Image/${directory}`)
+      const isImage = selectedFile.type.startsWith('image')
+      const directory = isImage
+        ? `${profile?.user_id}/${new Date().toISOString()}/${generateRandomString(20)}`
+        : `${profile?.user_id}/${new Date().toISOString()}/${selectedFile.name}`
+      const storageRef = isImage
+        ? ref(storage, `/Message_Image/${directory}`)
+        : ref(storage, `/Message_File/${directory}`)
       // progress can be paused and resumed. It also exposes progress updates.
       // Receives the storage reference and the file to upload.
-      const uploadTask = uploadBytesResumable(storageRef, file)
+      const uploadTask = uploadBytesResumable(storageRef, selectedFile)
 
       uploadTask.on(
         'state_changed',
@@ -62,7 +140,7 @@ export default function Footer() {
               message: {
                 temp_id: convertToDateTimeServer(new Date()),
                 room_id: roomInfo?.id,
-                message_type: 'Image',
+                message_type: isImage ? 'Image' : 'File',
                 content: directory,
                 root_message_id: null
               }
@@ -156,11 +234,22 @@ export default function Footer() {
       fileInputRef.current.click()
     }
   }
+  const handleOpenFileSelection = () => {
+    if (fileSelectionRef.current) {
+      fileSelectionRef.current.click()
+    }
+  }
 
   return (
     <div className='grow-1 flex items-center overflow-hidden bg-white' style={{ minHeight: height > 60 ? height : 60 }}>
-      <div className='mx-4 text-primary hover:cursor-pointer hover:text-blue-600' onClick={handleOpenImgSelection}>
+      <div className='ml-4 mr-2 text-primary hover:cursor-pointer hover:text-blue-600' onClick={handleOpenImgSelection}>
         <AddCircleRoundedIcon sx={{ fontSize: '28px' }} />
+      </div>
+      <div
+        className='ml-2 mr-4 text-primary hover:cursor-pointer hover:text-blue-600'
+        onClick={handleOpenFileSelection}
+      >
+        <AttachFileIcon sx={{ fontSize: '28px' }} />
       </div>
       <div className='my-2 flex grow items-center rounded-xl bg-gray-100 px-2 '>
         <textarea
@@ -177,7 +266,8 @@ export default function Footer() {
       <div className='mx-4 text-primary hover:cursor-pointer hover:text-blue-700' onClick={handleSubmit}>
         <SendRoundedIcon sx={{ fontSize: '28px' }} />
       </div>
-      <input type='file' accept='image/*' onChange={handleChange} ref={fileInputRef} className='hidden' />
+      <input type='file' accept='image/*' onChange={handleChangeFile} ref={fileInputRef} className='hidden' />
+      <input type='file' accept='*' onChange={handleChangeFile} ref={fileSelectionRef} className='hidden' />
     </div>
   )
 }
