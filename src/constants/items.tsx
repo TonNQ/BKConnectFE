9e10f3579def3ab0e59df992334601cad53841a1
TonNQ <nguyenquoctoan12122003@gmail.com>
2023-12-31 13:01:49 +0700

    change sidebar

--- src/constants/items.tsx
+++ src/constants/items.tsx
@@ -2,9 +2,6 @@ import MessageOutlinedIcon from '@mui/icons-material/MessageOutlined'
 import MessageIcon from '@mui/icons-material/Message'
 import PeopleAltOutlinedIcon from '@mui/icons-material/PeopleAltOutlined'
 import PeopleAltIcon from '@mui/icons-material/PeopleAlt'
-import ClassOutlinedIcon from '@mui/icons-material/ClassOutlined'
-import ClassIcon from '@mui/icons-material/Class'
-import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined'
 import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone'
 
 export const DashboardOutlinedIcon = [
@@ -22,23 +19,10 @@ export const DashboardOutlinedIcon = [
   },
   {
     index: 2,
-    title: 'Lớp học',
-    icon: <ClassOutlinedIcon sx={{ color: 'white', fontSize: '30px' }} />,
-    onlySideComponent: false
-  },
-  {
-    index: 3,
     title: 'Thông báo',
     icon: <NotificationsNoneIcon sx={{ color: 'white', fontSize: '30px' }} />,
     onlySideComponent: true,
     toggleComponent: 'notification'
-  },
-  {
-    index: 4,
-    title: 'Cài đặt',
-    icon: <SettingsOutlinedIcon sx={{ color: 'white', fontSize: '30px' }} />,
-    onlySideComponent: true,
-    toggleComponent: 'setting'
   }
 ]
 
@@ -55,17 +39,7 @@ export const DashboardFilledIcon = [
   },
   {
     index: 2,
-    title: 'Lớp học',
-    icon: <ClassIcon sx={{ color: 'white', fontSize: '30px', fontWeight: '400' }} />
-  },
-  {
-    index: 3,
     title: 'Thông báo',
     icon: <NotificationsNoneIcon sx={{ color: 'white', fontSize: '30px' }} />
-  },
-  {
-    index: 4,
-    title: 'Cài đặt',
-    icon: <SettingsOutlinedIcon sx={{ color: 'white', fontSize: '30px' }} />
   }
 ]
